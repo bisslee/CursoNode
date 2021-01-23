efbc503d19d79999af7bbf8c691dbd942ed15764
Ivana Batista dos Santos <bisslee@gmail.com>
2021-01-23 01:26:30 -0300

    packge

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require("http");
+const host = "127.0.0.1";
+
+const port = 3000;
+
+const server = http.createServer((req,res)=>{
+    res.statusCode=200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hey mundo! \n A Biss vai abalar!');
+
+});
+
+server.listen(port,host,()=>{
+    console.log(`server rodando em http://${host}:${port}`)
+});
+
